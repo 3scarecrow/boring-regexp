062d67acac075b2395dce655f1977bac94a362c9
sunjunhao <1648314523@qq.com>
2020-03-29 22:38:08 +0800

    docs: :tada: first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: '正则表达式',
+  description: 'Regexp',
+  base: '/Undefined/',
+  themeConfig: {
+    repo: 'https://github.com/3scarecrow/Undefined',
+    docsDir: 'docs',
+    lastUpdated: '上次更新'
+  }
+}
