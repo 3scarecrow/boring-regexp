1fbeaf8f6326884118ea3dea91b0198a6f1b527b
sunjunhao <1648314523@qq.com>
2020-03-29 23:05:41 +0800

    chore: update doc

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<3scarecrow>/<Undefined>.git master:gh-pages
+git push -f git@github.com:<3scarecrow>/<boring-regexp>.git master:gh-pages
 
 cd -
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   title: '正则表达式',
   description: 'Regexp',
-  base: '/Undefined/',
+  base: '/boring-regexp/',
   themeConfig: {
-    repo: 'https://github.com/3scarecrow/Undefined',
+    repo: 'https://github.com/3scarecrow/boring-regexp',
     docsDir: 'docs',
     lastUpdated: '上次更新'
   }
